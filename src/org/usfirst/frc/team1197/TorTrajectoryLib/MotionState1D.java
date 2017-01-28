46e6d1fa29eb4be6840f1945ff612c00b674d20d
Joe Loughboro <jwloughb@ucsc.edu>
2017-01-27 16:56:20 -0800

    Created MotionState1D class

---
+++ src/org/usfirst/frc/team1197/TorTrajectoryLib/MotionState1D.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1197.TorTrajectoryLib;
+
+public class MotionState1D {
+	public double pos;
+	public double vel;
+	public double acc;
+	public MotionState1D(double p, double v, double a){
+		pos = p;
+		vel = v;
+		acc = a;
+	}
+}
