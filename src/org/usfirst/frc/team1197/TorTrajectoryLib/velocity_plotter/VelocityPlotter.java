61b973a3dd613e634b37f921b2e159f664ef2a8f
Joe Loughboro <jwloughb@ucsc.edu>
2017-01-28 21:26:11 -0800

    Now it even displays a graph!

--- src/org/usfirst/frc/team1197/TorTrajectoryLib/velocity_plotter/VelocityPlotter.java
+++ src/org/usfirst/frc/team1197/TorTrajectoryLib/velocity_plotter/VelocityPlotter.java
@@ -4,15 +4,17 @@ import org.usfirst.frc.team1197.TorTrajectoryLib.LinearTrajectory;
 import org.usfirst.frc.team1197.TorTrajectoryLib.velocity_plotter.VelocityGraph.motionType;
 
 public class VelocityPlotter {
-	final static double dt = 0.005;
-	final static LinearTrajectory trajectory = new LinearTrajectory(1.0);
-	final static VelocityGraph graph = new VelocityGraph(trajectory, motionType.Translation);
 
 	public static void main(String[] args) {
+		double dt = 0.005;
 		long time;
 		double pos;
 		double vel;
 		double acc;
+
+		LinearTrajectory trajectory = new LinearTrajectory(1.0);
+		VelocityGraph graph = new VelocityGraph(trajectory, motionType.Translation);
+        graph.display();
 
 		for (double t = 0; t <= trajectory.totalTime(); t+=dt){
 			time = (long)(t*1000);
