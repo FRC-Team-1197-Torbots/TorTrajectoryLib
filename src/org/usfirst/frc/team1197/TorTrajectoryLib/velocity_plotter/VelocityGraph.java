ea608b496241140b182c689cb3e957deceb42e06
Joe Loughboro <jwloughb@ucsc.edu>
2017-01-28 21:23:08 -0800

    VelocityPlotter compiles!

--- src/org/usfirst/frc/team1197/TorTrajectoryLib/velocity_plotter/VelocityGraph.java
+++ src/org/usfirst/frc/team1197/TorTrajectoryLib/velocity_plotter/VelocityGraph.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team1197.TorTrajectoryLib.velocity_plotter;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
--- src/org/usfirst/frc/team1197/TorTrajectoryLib/velocity_plotter/VelocityPlotter.java
+++ src/org/usfirst/frc/team1197/TorTrajectoryLib/velocity_plotter/VelocityPlotter.java
@@ -1,10 +1,28 @@
 package org.usfirst.frc.team1197.TorTrajectoryLib.velocity_plotter;
 
+import org.usfirst.frc.team1197.TorTrajectoryLib.LinearTrajectory;
+import org.usfirst.frc.team1197.TorTrajectoryLib.velocity_plotter.VelocityGraph.motionType;
+
 public class VelocityPlotter {
+	final static double dt = 0.005;
+	final static LinearTrajectory trajectory = new LinearTrajectory(1.0);
+	final static VelocityGraph graph = new VelocityGraph(trajectory, motionType.Translation);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		long time;
+		double pos;
+		double vel;
+		double acc;
 
+		for (double t = 0; t <= trajectory.totalTime(); t+=dt){
+			time = (long)(t*1000);
+			pos = trajectory.lookUpPosition(time);
+			vel = trajectory.lookUpVelocity(time);
+			acc = trajectory.lookUpVelocity(time);
+			graph.position.add(t, pos);
+			graph.velocity.add(t, vel);
+			graph.acceleration.add(t, acc);
+	    }
 	}
 
 }
